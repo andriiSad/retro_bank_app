16fc8401c6a924c44ad3b56ea9931e36a520724c
andriySad <waterpolomen2@gmail.com>
2023-10-01 17:59:58 +0200

    added new screens to routing

--- lib/core/services/router/router.dart
+++ lib/core/services/router/router.dart
@@ -1,8 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart' as firebase_ui;
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:retro_bank_app/core/common/views/page_under_construction_view.dart';
+import 'package:retro_bank_app/core/extensions/context_extension.dart';
 import 'package:retro_bank_app/core/services/injection_container/injection_container.dart';
 import 'package:retro_bank_app/core/utils/constants.dart';
+import 'package:retro_bank_app/src/auth/data/models/local_user_model.dart';
+import 'package:retro_bank_app/src/auth/presentation/bloc/auth_bloc.dart';
+import 'package:retro_bank_app/src/auth/presentation/views/sign_in_screen.dart';
+import 'package:retro_bank_app/src/auth/presentation/views/sign_up_screen.dart';
 import 'package:retro_bank_app/src/dashboard/presentation/views/dashboard_screen.dart';
 import 'package:retro_bank_app/src/on_boarding/presentation/cubit/on_boarding_cubit.dart';
 import 'package:retro_bank_app/src/on_boarding/presentation/views/on_boarding_screen.dart';
--- lib/core/services/router/router_main.dart
+++ lib/core/services/router/router_main.dart
@@ -14,39 +14,58 @@ Route<dynamic> generateRoute(RouteSettings settings) {
               create: (_) => serviceLocator<OnBoardingCubit>(),
               child: const OnBoardingScreen(),
             );
-          } else {
-            return const DashboardScreen();
+          } else if (serviceLocator<FirebaseAuth>().currentUser != null) {
+            final user = serviceLocator<FirebaseAuth>().currentUser!;
+            final localUser = LocalUserModel(
+              id: user.uid,
+              email: user.email ?? '',
+              username: user.displayName ?? '',
+              photoUrl: user.photoURL ?? '',
+              cards: const [],
+            );
+            context.userProvider.initUser(localUser);
+            return BlocProvider(
+              create: (_) => serviceLocator<AuthBloc>(),
+              child: const DashboardScreen(),
+            );
           }
+          return BlocProvider(
+            create: (_) => serviceLocator<AuthBloc>(),
+            child: const SignInScreen(),
+          );
         },
+        settings: settings,
+      );
+    case SignInScreen.routeName:
+      return _pageBuilder(
+        (_) => BlocProvider(
+          create: (_) => serviceLocator<AuthBloc>(),
+          child: const SignInScreen(),
+        ),
+        settings: settings,
+      );
+    case SignUpScreen.routeName:
+      return _pageBuilder(
+        (_) => BlocProvider(
+          create: (_) => serviceLocator<AuthBloc>(),
+          child: const SignUpScreen(),
+        ),
         settings: settings,
       );
     case DashboardScreen.routeName:
       return _pageBuilder(
-        (_) => const DashboardScreen(),
+        (_) => BlocProvider(
+          create: (_) => serviceLocator<AuthBloc>(),
+          child: const DashboardScreen(),
+        ),
         settings: settings,
       );
-    // case SignInScreen.routeName:
-    //   return _pageBuilder(
-    //     (_) => BlocProvider(
-    //       create: (_) => serviceLocator<AuthBloc>(),
-    //       child: const SignInScreen(),
-    //     ),
-    //     settings: settings,
-    //   );
-    // case SignUpScreen.routeName:
-    //   return _pageBuilder(
-    //     (_) => BlocProvider(
-    //       create: (_) => serviceLocator<AuthBloc>(),
-    //       child: const SignUpScreen(),
-    //     ),
-    //     settings: settings,
-    //   );
 
-    // case '/forgot_password':
-    //   return _pageBuilder(
-    //     (_) => const firebase_ui.ForgotPasswordScreen(),
-    //     settings: settings,
-    //   );
+    case '/forgot_password':
+      return _pageBuilder(
+        (_) => const firebase_ui.ForgotPasswordScreen(),
+        settings: settings,
+      );
     default:
       return _pageBuilder(
         (_) => const PageUnderConstructionView(),
