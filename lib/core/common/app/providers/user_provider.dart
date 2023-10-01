1d5c3b4f2459725165eed00b1f653d6158ca300f
andriySad <waterpolomen2@gmail.com>
2023-10-01 18:01:09 +0200

    added userProvider

--- lib/core/common/app/providers/user_provider.dart
+++ lib/core/common/app/providers/user_provider.dart
@@ -1,17 +1,17 @@
-// import 'package:flutter/foundation.dart';
-// import 'package:tdd_courses_app/src/auth/data/models/user_model.dart';
+import 'package:flutter/foundation.dart';
+import 'package:retro_bank_app/src/auth/data/models/local_user_model.dart';
 
-// class UserProvider extends ChangeNotifier {
-//   LocalUserModel? _user;
+class UserProvider extends ChangeNotifier {
+  LocalUserModel? _user;
 
-//   LocalUserModel? get user => _user;
+  LocalUserModel? get user => _user;
 
-//   set user(LocalUserModel? user) {
-//     if (_user != user) _user = user;
-//     Future.delayed(Duration.zero, notifyListeners);
-//   }
+  set user(LocalUserModel? user) {
+    if (_user != user) _user = user;
+    Future.delayed(Duration.zero, notifyListeners);
+  }
 
-//   void initUser(LocalUserModel? user) {
-//     if (_user != user) _user = user;
-//   }
-// }
+  void initUser(LocalUserModel? user) {
+    if (_user != user) _user = user;
+  }
+}
