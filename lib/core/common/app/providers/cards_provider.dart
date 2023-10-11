27c9564e849a7f2c39ef62fbe92fec0c4c5718d7
andriySad <waterpolomen2@gmail.com>
2023-10-11 12:06:18 +0200

    changed to provide entities instead of models

--- lib/core/common/app/providers/cards_provider.dart
+++ lib/core/common/app/providers/cards_provider.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/foundation.dart';
-import 'package:retro_bank_app/src/auth/data/models/credit_card_model.dart';
+import 'package:retro_bank_app/src/auth/domain/entities/credit_card.dart';
 
 class CardsProvider extends ChangeNotifier {
-  List<CreditCardModel>? _cards;
+  List<CreditCard>? _cards;
 
-  List<CreditCardModel>? get cards => _cards;
+  List<CreditCard>? get cards => _cards;
 
   //TODO(create extension for deep list Comparasion)
-  set cards(List<CreditCardModel>? cards) {
+  set cards(List<CreditCard>? cards) {
     if (!listEquals(_cards, cards)) {
       _cards = cards;
       Future.delayed(Duration.zero, notifyListeners);
